3f1e1348f7bb4fe836d74cf2fd1ae0a2690cb51e
aparvin2k <aparvin2005@gmail.com>
2017-06-15 10:06:51 -0700

    Adding index, image, css, and js files

---
+++ assets/javascript/game.js
@@ -0,0 +1,114 @@
+var words = ["avengers", "tmnt", "captain america", "iron man", "star wars", "the dark knight", "jurassic park", "titanic", "finding nemo", "transformers", "spider man", "forrest gump",
+"harry potter and the deathly hollows", "pirates of the caribbean", "frozen", "inception", "shrek", "guardians of the galaxy", "the sixth sense", "beverly hills cop", "the lord of the rings", "rocky", "skyfall", "the blind side", "the incredibles", "home alone" ];
+var word = "";
+var blanks = [];
+var userGuess = "";
+var guessBank = [];
+var message = "";
+var guessesLeft = 0;
+var remainingLetters = 0;
+var gameend = false;
+var gameover = document.querySelector("#gameover");
+
+	function randomWord(){
+		word = words[Math.floor(Math.random() * words.length)];
+		return;
+	};
+
+	function setBlanks(){
+		blanks = [];
+	 	for (var i = 0; i < word.length; i++) {
+	    blanks[i] = "_";
+	  	}
+		document.getElementById("blanks").innerHTML = blanks.join(" ");
+		return;
+	};
+
+	function setBoard(){
+		guessesLeft = 10;
+        guessBank.length = 0;
+        gameend = false;
+		document.getElementById("remaining").innerHTML = "Remaining Guesess: " + guessesLeft;
+        document.getElementById("guess").innerHTML = guessBank;
+		document.getElementById("start").style.visibility = "hidden";
+        document.getElementById("gameover").style.visibility = "hidden";
+		document.getElementById("img-tnt").style.visibility = "visible";
+		document.getElementById("instruction").innerHTML = "Come on now, you can do better than that";
+	};
+
+    function wrongLetter() {
+        message = "Sorry, no " + userGuess + "'s." + " Do you want this guy to die?";
+        guessesLeft--;
+        document.getElementById("remaining").innerHTML = "Remaining Guesses: " + guessesLeft;
+    };
+
+    function win() {
+        message = "You Guessed " + word + "." + " You know your Blockbusters!";
+        document.getElementById("btn_name").innerHTML = "Play Again";
+        document.getElementById("start").style.visibility = "visible";
+        document.getElementById("saved").style.visibility = "visible";
+
+    };
+
+    function lose() {
+        message = "GAME OVER";
+        document.getElementById("btn_name").innerHTML = "Try Again";
+        document.getElementById("img-tnt").style.visibility = "hidden";
+        document.getElementById("gameover").style.visibility = "visible";
+        document.getElementById("start").style.visibility = "visible";
+    };
+
+	document.getElementById("start").onclick = function() {
+		randomWord();
+		setBlanks();
+		setBoard();
+	};
+
+	document.onkeyup = function(event) {
+
+	userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    guessBank.push(userGuess);
+	message = "";
+
+  	if (gameend === true) {
+      message ="You might want to retry.";
+  	} else {
+
+        var i=0;
+        for (i = 0; i < word.length; i++) {
+            if (word[i] === userGuess) {
+                blanks[i] = userGuess;
+                message = "Nice job. " + "You guessed an " + userGuess + " correctly.";
+            }
+        }
+
+        remainingLetters = blanks.length;
+        for (i = 0; i < blanks.length; i++) {
+            if (blanks[i] !== '_') {
+            remainingLetters -= 1;
+            }
+        }
+
+        if (message === "") {
+            wrongLetter();
+        }
+
+        if (remainingLetters == 0) {
+            gameend = true;
+            win();
+     	}
+
+        if (guessesLeft === 0) {
+            gameend = true;
+            lose();
+        }
+        document.getElementById("blanks").innerHTML = blanks.join(" ");
+        document.getElementById("guess").innerHTML = guessBank.join(", ");
+  }
+  document.getElementById("instruction").innerHTML = message;
+};
+
+
+
+
+
